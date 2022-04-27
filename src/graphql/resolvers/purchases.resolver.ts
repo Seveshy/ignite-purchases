7cd259be23f2f5fb056454298c651ea3784ca9c6
Daniel Major <daniel.alves@skytef.com.br>
2022-04-26 21:18:44 -0300

    Retornando dados unicos na tabela de purchase

--- src/graphql/resolvers/purchases.resolver.ts
+++ src/graphql/resolvers/purchases.resolver.ts
@@ -1,18 +1,30 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
 
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { ProductService } from 'src/services/products.service';
 import { PurchasesService } from 'src/services/purchases.service';
+import { Product } from '../models/product';
 
 import { Purchase } from '../models/purchase';
 
-@Resolver()
+@Resolver(() => Purchase)
 export class PurchasesResolver {
-  constructor(private purchaseService: PurchasesService) {}
+  constructor(
+    private purchaseService: PurchasesService,
+    private productsService: ProductService
+    ) {}
 
   @Query(() => [Purchase])
   @UseGuards(AuthorizationGuard)
   purchases() {
     return this.purchaseService.listAllPurchases();
+  }
+
+  @ResolveField(() => Product)
+  product(
+    @Parent() purchase: Purchase,
+  ) {
+    return this.productsService.getProductById(purchase.productId);
   }
 }
